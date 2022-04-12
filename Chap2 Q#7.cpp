ad5e3170d7abcd29c0346c106f6d45e0adb96727
Muhammad-Karar <102608977+Muhammad-Karar@users.noreply.github.com>
2022-04-12 09:43:32 -0700

    Add files via upload

---
+++ Chap2 Q#7.cpp
@@ -0,0 +1,24 @@
+/*Question No 5:
+Write a program to check whether a triangle is valid or not, 
+when the three angles of the triangle are entered through the 
+keyboard. A triangle is valid if the sum of all the three angles 
+is equal to 180 degrees.*/
+
+#include<stdio.h>
+int main()
+{
+	int angle_1,angle_2,angle_3,sum;
+	printf("\nEnter Three Angles of a Triangles : ");
+	scanf("%d %d %d",&angle_1,&angle_2,&angle_3);
+	
+	sum=angle_1+angle_2+angle_3;
+	printf("\nThe Sum of Angles is = %d\n",sum);
+	printf("So");
+	
+	if(sum==180)
+	printf("\nThe Triangle is valid: ");
+	
+	else
+	printf("\nThe Triangle is Not valid: ");
+	
+}
